d7ca5586aa733756ffdcd85deb32b02b91d30693
Ruben Suarez <rubensuarez@gmail.com>
2024-09-19 21:28:33 -0500

    Subiendo DB jrparking a proyecto

---
+++ controller/login.php
@@ -0,0 +1,16 @@
+<?php
+echo "estas en control del Login <br/>"
+
+require_once ("/controller/config.php");
+
+// require_once("ConfigDb.class.php");
+// $bd = new Config();
+// $conn = $bd->conexion();
+$sql = "INSERT INTO `tbestado`(`ID_ESTADO`, `DESCRIPCION_EST`) VALUES (null, '3','')"
+$stmt = $conn -> prepare ($sql);
+if ($stmt -> execute()){
+     echo "SQL realizada correctamente";
+} else {
+    echo "SQL no aplicada";
+}
+?>
