24e4cfae2f018f99c007d5c5454b4eab8f319b05
Ruben Suarez <rubensuarez@gmail.com>
2024-10-22 21:36:26 -0500

    Se crea interfaz datos-moto y se da funcionalidad

--- js/main.js
+++ js/main.js
@@ -16,7 +16,8 @@ document.addEventListener("click", (e)=>{
     //console.log(e.target.matches(".btn-ingresar"))
     if(e.target.matches(".btn-ingresar")) link("login.html")
     if(e.target.matches("#btn-auto")) link("datos_auto.html")
-    if(e.target.matches("#btn-bici")) link("ingredatosbici.html")
+    if(e.target.matches("#btn-bici")) link("datos-bici.html")
+    if(e.target.matches("#btn-moto")) link("datos-moto.html")
     if(e.target.matches("#nueva-cuenta")) link("creaciondecuenta.html")
     if(e.target.matches("#btncerrarsesion")) salida()
     
